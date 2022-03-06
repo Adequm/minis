faafdace51301facf39f0e1b8ef2b4452911162e
Adequm <adequm@bk.ru>
2022-03-06 19:30:58 +0300

    add translate.mixin

--- src/App.vue
+++ src/App.vue
@@ -61,6 +61,7 @@ import _ from 'lodash';
 import minisMixin from './mixins/minis.mixin';
 import resizeMixin from './mixins/resize.mixin';
 import faviconMixin from './mixins/favicon.mixin';
+import translateMixin from './mixins/translate.mixin';
 import Icon from './components/app/Icon';
 import SettingsDesktop from './components/app/SettingsDesktop';
 import SettingsMobile from './components/app/SettingsMobile';
@@ -78,7 +79,7 @@ export default {
     Icon,
   },
 
-  mixins: [minisMixin, resizeMixin, faviconMixin],
+  mixins: [minisMixin, resizeMixin, faviconMixin, translateMixin],
 
   data: () => ({
     lodash: _,
