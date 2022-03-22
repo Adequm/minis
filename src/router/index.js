55b90a4e7fc0037bba8948c2ac09124b0734c826
Adequm <adequm@bk.ru>
2022-03-22 03:43:17 +0300

    init framesCore

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+  },
+];
+
+if(process.env.NODE_ENV === 'development') {
+  routes.push(...[
+    {
+      path: '/2048',
+      name: '2048',
+      component: () => import('../../../2048/src/App.vue'),
+    },
+    {
+      path: '/calculator',
+      name: 'Calculator',
+      component: () => import('../../../calculator/src/App.vue'),
+    },
+    {
+      path: '/comparison',
+      name: 'Comparison',
+      component: () => import('../../../comparison/src/App.vue'),
+    },
+    {
+      path: '/docx-template',
+      name: 'DocxTemplate',
+      component: () => import('../../../docx-template/src/App.vue'),
+    },
+    {
+      path: '/wishes',
+      name: 'Wishes',
+      component: () => import('../../../wishes/src/App.vue'),
+    },
+  ]);
+};
+
+
+export default new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes,
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,230 @@
+<template>
+  <div 
+    class="container" 
+    :class="{ fullscreen: isFullscreen || !isWidthMore768 || isFrame }"
+    :style="{ 
+      height: `${ innerHeight }px`, 
+      maxHeight: isDesktop ? `${ containerHeight }px` : '100vh',
+    }"
+  >
+    <AppLoader v-if="!isPageLoad" :size="100" rotate/>
+
+    <div v-else class="frames__container">
+      <div 
+        v-for="(minis, minisIndex) of links"
+        :key="`${minis}_${minisIndex}`"
+        v-show="!isFullscreen && isWidthMore768 || framePageIndex == minisIndex"
+        class="minis__wrapper"
+        :style="{ 
+          width: `${ appWidth }px`,
+          maxWidth: `${ isFullscreen || !isWidthMore768 || isFullscreenInFrame ? appWidth : containerWidth }px`,
+        }"
+      >
+        <SettingsDesktop
+          v-if="!minisIndex && isDesktop && !isFrame"
+          :themeIcon="themeMain.icon"
+          :appHeight="appHeight"
+          :appWidth="appWidth"
+          :translate="translate"
+          :translateDef="translateDef"
+          :showHints="minisHints"
+          v-model="isClosedSettings"
+          @switchHints="switchHints"
+          @switchTheme="switchTheme"
+          @switchLang="switchLang"
+          @switchFullscreen="$store.commit(switchFullscreenKey)"
+        />
+
+        <LayoutFrame
+          :isResize="!!startResizeX"
+          :key="`${minisIndex}_${minis}_${isFullscreen || !isWidthMore768}`"
+          :link="`${ domen + minis }?isFullscreen=${ isFullscreen || !isWidthMore768 }&index=${ minisIndex }`"
+          :style="{ filter: openedModalName ? 'blur(2px)' : 'none' }"
+          @load="initFrameSettingsWatcher(minisIndex)"
+        />
+
+        <AppModal 
+          v-if="!isFullscreen || framePageIndex == minisIndex"
+          v-model="openedModalName"
+          :isRoundedBorder="isWidthMore768 && !isFullscreen"
+        >
+          <SettingsMobile 
+            v-if="openedModalName == 'settings'"
+            :themeIcon="themeMain.icon"
+            :isWidthMore768="isWidthMore768"
+            :showArrows="links.length > 1"
+            :title="translateChain(minis)('title')()"
+            @switchTheme="switchTheme"
+            @switchLang="switchLang"
+            @switchFullscreen="$store.commit(switchFullscreenKey)"
+            @clickToArrow="clickToArrow"
+          />
+        </AppModal>
+
+        <div 
+          v-if="isDesktop" 
+          class="resize_button" 
+          @mousedown.prevent="startResize"
+          @dblclick.prevent="autoResize"
+        />
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { AppLoader, AppModal, SettingsDesktop, SettingsMobile, LayoutFrame } from '@minis-core/components';
+import { minisMixin, resizeMixin, faviconMixin, translateMixin, coreMixin, frameMixin } from '@minis-core/mixins';
+
+export default {
+  components: {
+    AppModal,
+    AppLoader,
+    SettingsDesktop,
+    SettingsMobile,
+    LayoutFrame,
+  },
+
+  mixins: [
+    minisMixin, 
+    resizeMixin, 
+    faviconMixin, 
+    translateMixin, 
+    coreMixin,
+    frameMixin,
+  ],
+
+  beforeMount() {
+    this.links = ['calculator', 'comparison'];
+  },
+};
+</script>
+
+<style lang="scss">
+::-webkit-scrollbar {
+  width: 5px; 
+  background-color: var(--content-bg-color);
+}
+::-webkit-scrollbar-thumb {
+  width: 5px; 
+  background-color: var(--special-color); 
+}
+::selection {
+  background: var(--special-color); 
+}
+
+button {
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  border: none;
+  outline: none;
+  color: inherit;
+}
+
+body {
+  font-family: 'Noto Sans', sans-serif;
+  margin: 0;
+  display: flex;
+  background-image: url("../assets/confetti.svg");
+  background-repeat: repeat;
+  background-position: center;
+  background-color: #333;
+  min-height: 100vh;
+  overflow: hidden;
+
+  .container {
+    width: 100vw;
+    margin: 0 auto;
+
+    .frames__container {
+      height: 100%; 
+      display: flex; 
+      justify-content: center; 
+      gap: 20px; 
+      flex-direction: row-reverse;
+    }
+
+    .minis__wrapper {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      box-shadow: 0 3px 0 2px var(--main-bg-color);
+      border-radius: 10px;
+      box-sizing: border-box;
+
+      .confirmation {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        font-size: 18px;
+        strong {
+          color: var(--special-color);
+        }
+        button {
+          margin-top: 10px;
+          border-radius: 10px;
+          padding: 10px;
+          background: var(--special-color);
+          cursor: pointer;
+          &:hover {
+            opacity: .8;
+          }
+        }
+      }
+
+      .minis {
+        display: flex;
+        position: absolute;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        color: var(--special-color);
+        opacity: .5;
+        cursor: pointer;
+        transition: opacity .2s;
+        text-decoration: none;
+        border: none;
+        outline: none;
+        width: 50px;
+        height: 30px;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        z-index: 5;
+        user-select: none; 
+        &:hover { opacity: 1; }
+        &:focus { color: var(--text-color); }
+      }
+
+      .resize_button {
+        position: absolute;
+        display: block;
+        width: 10px;
+        height: 10px;
+        background: var(--special-color);
+        transform: translateY(-100%);
+        right: 0;
+        top: 100%;
+        z-index: 101;
+        clip-path: polygon(100% 0, 100% 100%, 0 100%);
+        border-radius: 0 0 10px 0;
+        cursor: se-resize;
+      }
+    }
+
+  }
+}
+
+@keyframes rotate {
+  to { transform: rotate(360deg); }
+}
+
+@media screen and (min-width: 768px) {
+  body .container {
+    margin: auto;
+  }
+}
+</style>
